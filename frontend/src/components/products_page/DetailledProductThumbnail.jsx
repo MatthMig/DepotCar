8543e812a27d60cd3a7cf8fb063338f74b63dff8
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-04-20 22:31:59 +0200

    carousel display change for detailed product thumbnail

--- frontend/src/components/products_page/DetailledProductThumbnail.jsx
+++ frontend/src/components/products_page/DetailledProductThumbnail.jsx
@@ -1,41 +1,26 @@
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import React from "react";
+import OwlCarousel from "react-owl-carousel";
 import DetailledProductDescription from "./DetailledProductDescription";
 
 function DetailledProductThumbnail({ images, title, price, oldPrice, data }) {
-    // Function to chunk the images array into groups of 3
-    const chunkArray = (arr, size) => {
-        return arr.reduce((chunks, el, i) => {
-            if (i % size === 0) {
-                chunks.push([el]);
-            } else {
-                chunks[chunks.length - 1].push(el);
-            }
-            return chunks;
-        }, []);
-    };
-
-    // Chunk the images array into groups of 3
-    const imageChunks = chunkArray(images.slice(1), 3);
-
     return (
         <div className="row">
             <div className="col-md-6 col-xs-12">
                 <div>
-                    <img src={images[0]} alt="" className="img-fluid wc-image" />
-                </div>
-                <br />
-                {imageChunks.map((chunk, rowIndex) => (
-                    <div className="row" key={rowIndex}>
-                        {chunk.map((image, index) => (
-                            <div className="col-sm-4 col-xs-6" key={index}>
-                                <div>
-                                    <img src={image} alt="" className="img-fluid" />
-                                </div>
-                                <br />
+                    <OwlCarousel 
+                        className="owl-theme"
+                        loop
+                        items={1} 
+                    >
+                        {images.map((image, index) => (
+                            <div key={index}>
+                                <img src={image} alt="" className="img-fluid wc-image" />
                             </div>
                         ))}
-                    </div>
-                ))}
+                    </OwlCarousel>
+                </div>
             </div>
             <div className="col-md-6 col-xs-12">
                 <DetailledProductDescription
