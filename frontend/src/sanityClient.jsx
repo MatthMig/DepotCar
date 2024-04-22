4b6d835f6e131dd440c62029b63dfa2ec36db1bf
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-04-22 12:46:21 +0200

    Remove of unused fonts

--- frontend/src/sanityClient.jsx
+++ frontend/src/sanityClient.jsx
@@ -2,8 +2,8 @@ import { createClient } from "@sanity/client";
 
 export default createClient({
 
-  projectId: "xkojrun8", // find this at manage.sanity.io or in your sanity.json
+  projectId: "xkojrun8", 
 
-  dataset: "production", // this is from those question during 'sanity init'
+  dataset: "production",
 
 });
